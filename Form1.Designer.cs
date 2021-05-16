3eabfc26db97932e015512d30c51b2c1b2e190d7
Eleonorka19 <79839247+Eleonorka19@users.noreply.github.com>
2021-05-16 13:22:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace The_Bletchley__code_breakers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.Изчистване = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Отговор = new System.Windows.Forms.ListBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ОК = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInsert.Location = new System.Drawing.Point(278, 45);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Нова игра";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            // 
+            // Изчистване
+            // 
+            this.Изчистване.BackColor = System.Drawing.Color.Snow;
+            this.Изчистване.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Изчистване.Location = new System.Drawing.Point(422, 45);
+            this.Изчистване.Name = "Изчистване";
+            this.Изчистване.Size = new System.Drawing.Size(75, 23);
+            this.Изчистване.TabIndex = 1;
+            this.Изчистване.Text = "Изчистване";
+            this.Изчистване.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(32, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "4";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(130, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(32, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "5";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Отговор
+            // 
+            this.Отговор.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Отговор.FormattingEnabled = true;
+            this.Отговор.Location = new System.Drawing.Point(12, 162);
+            this.Отговор.Name = "Отговор";
+            this.Отговор.Size = new System.Drawing.Size(315, 199);
+            this.Отговор.TabIndex = 4;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtNumber.Location = new System.Drawing.Point(12, 136);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Колко цифри да има намисленото число?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.RosyBrown;
+            this.label2.Location = new System.Drawing.Point(9, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Вашето предложение";
+            // 
+            // ОК
+            // 
+            this.ОК.Location = new System.Drawing.Point(118, 136);
+            this.ОК.Name = "ОК";
+            this.ОК.Size = new System.Drawing.Size(32, 20);
+            this.ОК.TabIndex = 8;
+            this.ОК.Text = "ОК";
+            this.ОК.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(600, 388);
+            this.Controls.Add(this.ОК);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.Отговор);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Изчистване);
+            this.Controls.Add(this.btnInsert);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button Изчистване;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ListBox Отговор;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ОК;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Bletchley__code_breakers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] nums = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            int[] choseNum = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Random rd = new Random();
+                int randomIndex = rd.Next(0, 7);
+                choseNum[i] = randomIndex;
+            }
+        }
+    }
+}
